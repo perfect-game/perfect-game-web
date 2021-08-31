7ecc2ca3d1047f4f04a90ab144144b165f60c671
seonggukchoi <seonggukchoi.kr@gmail.com>
2021-09-01 02:48:00 +0900

    Update design

--- src/views/sign-in/sign-in.module.ts
+++ src/views/sign-in/sign-in.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
+import { CardModule } from '@app/components/card';
+
 import { SignInRoutingModule } from './sign-in.routing-module';
 import { SignInComponent } from './sign-in.component';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [SignInRoutingModule],
+  imports: [CardModule, SignInRoutingModule],
 })
 export class SignInModule {}
